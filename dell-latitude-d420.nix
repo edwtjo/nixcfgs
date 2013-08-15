bef12d32fc81e97897d4047c41c130a35f365037
Edward Tjörnhammar <ed@cflags.cc>
2013-08-15 13:39:26 +0200

    Adding hardware configuration

---
+++ dell-latitude-d420.nix
@@ -0,0 +1,15 @@
+# This is a generated file.  Do not modify!
+# Make changes to /etc/nixos/configuration.nix instead.
+{ config, pkgs, ... }:
+
+{
+  require = [
+    <nixos/modules/installer/scan/not-detected.nix>
+  ];
+
+  boot.initrd.kernelModules = [ "uhci_hcd" "ehci_hcd" "ata_piix" "firewire_ohci" "usb_storage" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nix.maxJobs = 2;
+}
