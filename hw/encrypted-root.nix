89d6d6d95b16812b37d4a18100ae6b1c75fe2a5d
Edward Tjörnhammar <ed@cflags.cc>
2013-08-16 22:01:35 +0200

    XTS support

--- hw/encrypted-root.nix
+++ hw/encrypted-root.nix
@@ -5,5 +5,6 @@
     { name = "cryptfs"; device = "/dev/sda3"; }
     { name = "cryptswap"; device = "/dev/sda2"; }
   ];
+  boot.initrd.luks.cryptoModules = ["aes" "sha256" "sha1" "xts"];
   boot.initrd.postMountCommands = "cryptsetup luksOpen --key-file /mnt-root/root/.swapkey /dev/sda2 cryptswap";
 }
