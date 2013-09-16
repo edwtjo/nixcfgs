e92e2381bdc771a38f8c14612267560ff00ca94c
Edward Tjörnhammar <ed@cflags.cc>
2013-09-16 20:01:25 +0200

    Encrypted root for tempest

--- hw/encrypted-root.nix
+++ hw/encrypted-root.nix
@@ -2,9 +2,8 @@
 
 {
   boot.initrd.luks.devices = [
-    { name = "cryptfs"; device = "/dev/sda3"; }
-    { name = "cryptswap"; device = "/dev/sda2"; }
+    { name = "cryptfs"; device = "/dev/sdb3"; }
   ];
   boot.initrd.luks.cryptoModules = ["aes" "sha256" "sha1" "xts"];
-  boot.initrd.postMountCommands = "cryptsetup luksOpen --key-file /mnt-root/root/.swapkey /dev/sda2 cryptswap";
+  boot.initrd.postMountCommands = "cryptsetup luksOpen --key-file /mnt-root/root/.swapkey /dev/sdb2 cryptswap";
 }
--- tempest.nix
+++ tempest.nix
@@ -3,24 +3,31 @@
 {
   require = [
     ./hw/clevo-p150hm.nix
+    ./hw/encrypted-root.nix
     ./sets/common.nix
     ./sets/laptop.nix
     ./sets/x11.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_3_10;
+  boot.kernelPackages = pkgs.linuxPackages_3_11;
 
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/sdb";
 
   fileSystems = [
-    { mountPoint = "/";
-      device = "/dev/sda3";
-      fsType = "jfs";
+    {
+      mountPoint = "/";
+      device = "/dev/mapper/cryptfs";
+      fsType = "ext4";
+    }
+    {
+      mountPoint = "/boot";
+      device = "/dev/sdb1";
+      fsType = "ext2";
     }
   ];
 
   swapDevices = [
-    { device = "/dev/sda2"; }
+    { device = "/dev/mapper/cryptswap"; }
   ];
 
   networking = {
