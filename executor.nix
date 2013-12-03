d45774af30f1376120f7d3751ac069268aae6ac9
Edward Tjörnhammar <ed@cflags.cc>
2013-12-03 06:41:32 +0100

    Executor, qemu testvm

---
+++ executor.nix
@@ -0,0 +1,59 @@
+{config, pkgs, ...}:
+
+{
+  require = [
+    ./hw/kvm.nix
+    ./sets/common.nix
+    ./sets/laptop.nix
+    ./sets/writing.nix
+    ./sets/x11.nix
+    ./sets/developer.nix
+    ./sets/tomcat.nix
+  ];
+
+  environment.nix = pkgs.nixUnstable;
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_3_12;
+    loader.grub = {
+      device = "/dev/vda";
+    };
+  };
+
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/vda1";
+      fsType = "ext2";
+    };
+
+    "/" = {
+      device = "/dev/vda3";
+      fsType = "jfs";
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/vda2"; }
+  ];
+
+  networking = {
+    hostName = "executor";
+    interfaceMonitor.enable = true;
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "150.227.50.1" ];
+    interfaces = {
+      name = "eth0";
+      ipAddress = "192.168.0.102";
+      subnetMask = "255.255.255.0";
+    };
+  };
+
+  services = {
+    cups = {
+      enable = true;
+    };
+    openvpn.enable = true;
+    services.samba.enable = true;
+    tomcat.enable = true;
+  };
+}
