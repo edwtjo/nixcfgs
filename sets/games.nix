107cc7f58e8976783c9e6e9bcb46ec6d733b3606
Edward Tjörnhammar <ed@cflags.cc>
2014-04-30 22:57:02 +0200

    game updates

--- sets/games.nix
+++ sets/games.nix
@@ -1,7 +1,24 @@
 {config, pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
+    bzflag
+    freeciv
+    gnuchess
+    lincity_ng
+    # minetest
+    nethack
+    openlierox
+    openttd
+    #scorched3d
+    superTux
+    superTuxKart
     steam
     steamChrootEnv
+    wesnoth
+    widelands
+    #tremulous
+    zangband
+    xboard
+    xonotic
   ];
 }
