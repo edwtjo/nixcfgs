9f31b7faa3d05ab476d4a256132de5939491a8c5
Edward Tjörnhammar <ed@cflags.cc>
2014-08-07 21:24:08 +0200

    SD card reader

--- hw/clevo-p150hm.nix
+++ hw/clevo-p150hm.nix
@@ -9,10 +9,14 @@
         # jfs on rootfs means we must have fsck
         cp -v ${pkgs.jfsutils}/sbin/fsck.jfs $out/bin
       '';
-      kernelModules = [ "ehci_hcd" "ahci" "xhci_hcd" "usb_storage" "jfs" ];
+      kernelModules = [ "ehci_hcd" "ahci" "xhci_hcd" "shpchp" "usb_storage" "jfs" ];
     };
-    kernelModules = [ "kvm-intel" "fuse" ];
-    extraModulePackages = [ pkgs.jfsutils ];
+    kernelModules = [ "kvm-intel" "fuse" "mmc_core" ];
+    extraModulePackages = [ pkgs.btrfsProgs ];
+    postBootCommands = ''
+      # enable SD card reader
+      echo 1 > /sys/bus/pci/rescan
+    '';
   };
 
   environment.systemPackages = with pkgs; [
