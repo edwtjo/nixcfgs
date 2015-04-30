6bed5ee4e66f8918d36ffe0505e0f017e4339ebb
Edward Tjörnhammar <ed@cflags.cc>
2015-04-30 21:05:57 +0200

    useful pythons

--- sets/math.nix
+++ sets/math.nix
@@ -1,8 +1,9 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
     gnuplot
     maxima
     octave
+    ( with pythonPackages; [ numpy scipy matplotlib ipython cvxopt ])
   ];
 }
