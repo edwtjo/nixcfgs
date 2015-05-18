e8620604313677717b19a775127ed13fcd702ed3
Edward Tjörnhammar <ed@cflags.cc>
2015-05-18 13:11:33 +0200

    ET phone home

---
+++ user/revvpn.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+  users.extraUsers.revvpn = {
+      createHome = true;
+      uid = 1900;
+      group = "revvpn";
+      openssh.authorizedKeys.keys = [
+        "/etc/nixos/user/revvpn.pub"
+      ];
+      home = "/home/revvpn";
+      shell = pkgs.zsh + "/bin/zsh";
+  };
+  users.extraGroups.revvpn.gid = 1900;
+
+  systemd.services.revvpn = {
+     description = "Reverse tunnel";
+     wantedBy = [ "multi-user.target" ];
+     after = [ "network.target" ];
+     serviceConfig = {
+       User = "revvpn";
+       Group = "revvpn";
+       Restart = "always";
+       RestartSec = "1200";
+       ExecStart = "${pkgs.openssh}/bin/ssh -NTC -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no revvpn@nexus.cube2.se -R 50022:localhost:22";
+     };
+   };
+}
