f26feee9638ce8d515d1c4564133fa5bb43671c3
Edward Tjörnhammar <ed@cflags.cc>
2015-05-22 05:12:01 +0200

    Change synchome to be a user service

--- modules/synchome.nix
+++ modules/synchome.nix
@@ -1,44 +1,54 @@
-{ config, lib, pkgs, ...}:
+{ config, lib, pkgs, ... }:
 
 with lib;
 
 let
 
   cfg = config.tjonix.synchome;
-  synchomeOptions = {
-    enable = mkOption {
-      default = false;
-    };
-    user = mkOption {
-      default = "nobody";
-    };
-    unison = mkOption {
-      default = pkgs.unison;
-    };
-  };
-  synchome = unison: pkgs.writeScriptBin "synchome" ''
-    #!/bin/sh
-    source ~/.keychain/$HOSTNAME-sh
-    export PATH=${pkgs.openssh}/bin:$PATH # since unison calls ssh
-    ${unison}/bin/unison $HOSTNAME -ui text -batch -prefer newer -times -logfile /tmp/unison-$USER-$(${pkgs.coreutils}/bin/date +%F-%T).log
-  '';
-in
-{
-  options.tjonix.synchome = synchomeOptions;
 
-  config = mkIf cfg.enable {
-    systemd.services."${cfg.user}-synchome" = {
-      description = "Synchronizes users HOME";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-        serviceConfig = {
-        User = cfg.user;
-        Group = cfg.user;
-        Type = "simple";
-        Restart = "always";
-        RestartSec = "1200";
-        ExecStart = "${synchome cfg.unison}/bin/synchome";
+  unisonUserService = unison: ({
+    "synchome-${replaceChars ["."] [""] unison.version}" = {
+    description = "Runs synchome in batch mode";
+    enable = true;
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      Type = "forking";
+      ExecStart = "${synchome unison}/bin/synchome-${unison.version}";
+      ExecStop = "${pkgs.procps}/bin/pkill unison";
+      RestartSec = "1200";
+      Restart = "always";
       };
     };
+  });
+
+  synchome = unison: pkgs.writeScriptBin ("synchome-"+unison.version) ''
+    #!/bin/sh
+    . ${config.system.build.setEnvironment}
+    source ~/.keychain/$HOSTNAME-sh
+    export PATH=${pkgs.openssh}/bin:$PATH # since unison calls ssh
+    ${pkgs.coreutils}/bin/nohup ${unison}/bin/unison $HOSTNAME -ui text -batch -prefer newer -times -logfile /tmp/unison-$USER-$(${pkgs.coreutils}/bin/date +%F-%T).log &
+  '';
+
+in
+
+{
+  options.tjonix.synchome = {
+    enable = mkEnableOption "Synchome User Service";
+    userService = mkOption {
+      default = true;
+      type = types.bool;
+      description = "Add user service";
+    };
+
+    type = with types; loaOf optionSet;
+    unisons = mkOption {
+      default = [];
+      type = with types; listOf package;
+    };
   };
-}
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = cfg.unisons;
+    systemd.user.services = mkIf cfg.userService (lib.fold (x: y: lib.recursiveUpdate x y) {} (map unisonUserService cfg.unisons));
+  };
+ }
