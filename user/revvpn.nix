84308c46bc4daff8444958cfeaa63bf96ebd1c8a
Edward Tjörnhammar <ed@cflags.cc>
2015-05-28 09:58:01 +0200

    also add reverse proxy port

--- user/revvpn.nix
+++ user/revvpn.nix
@@ -24,4 +24,19 @@
        ExecStart = "${pkgs.openssh}/bin/ssh -NTC -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no revvpn@nexus.cube2.se -R 50022:localhost:22";
      };
    };
+
+  systemd.services.revproxy = {
+     description = "Reverse Proxy";
+     wantedBy = [ "multi-user.target" ];
+     after = [ "network.target" ];
+     serviceConfig = {
+       User = "revvpn";
+       Group = "revvpn";
+       Restart = "always";
+       RestartSec = "1200";
+       ExecStart = "${pkgs.openssh}/bin/ssh -NTC -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no revvpn@nexus.cube2.se -R 51080:localhost:1080";
+     };
+   };
+
+
 }
